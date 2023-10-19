3e00ca1418f2e737349bb3e7959b116a4a287398
mdshahjhan <mdshahjha478877@gmail.com>
2023-10-20 01:11:28 +0600

    saju

--- src/Page/Login/Register.jsx
+++ src/Page/Login/Register.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
-import { Link,  } from "react-router-dom";
+import { Link, } from "react-router-dom";
 import { AuthContext } from "../Context/AuthProvider";
+import logo from '../../assets/assets/Tablet login-bro.png'
 
 const Register = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const {createUser,updateUserProfile} = useContext(AuthContext)
-    
+    const { createUser, updateUserProfile,googleSignIn } = useContext(AuthContext)
+
 
     const onSubmit = data => {
 
@@ -48,9 +49,22 @@ const Register = () => {
                     .catch(error => console.log(error))
             })
     };
+    // google sigin 
 
-   
+    const handleGoogleSIgnIn = () => {
+        googleSignIn()
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                // setAuthToken(user);
+            })
+            .catch(err => console.error(err));
     
+    }
+
+
+
+
 
     console.log(watch("example"));
     return (
@@ -60,9 +74,9 @@ const Register = () => {
             </Helmet>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Sign up now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <div className="text-center lg:text-left ml-10 pl-10">
+                        <img src={logo} alt=""></img>
+
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <form onSubmit={handleSubmit(onSubmit)} className="card-body">
@@ -110,12 +124,21 @@ const Register = () => {
                             </div>
                         </form>
                         <p className="text-center"><small>Already have an account <Link to="/login">Login</Link></small></p>
-                      
+
+                        <button onClick={handleGoogleSIgnIn} aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ri focus:ri dark:border-gray-400 focus:ri">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
+                                <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+                            </svg>
+                            <p>Login with Google</p>
+                        </button>
+
+                    </div>
+                        
+                       
                     </div>
                 </div>
-            </div>
-        </>
-    );
+            </>
+            );
 };
 
-export default Register;
+            export default Register;
