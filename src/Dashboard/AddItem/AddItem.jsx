dc20db25e23c226569f6d470c5f0665697f5bb66
mdshahjhan <mdshahjha478877@gmail.com>
2023-10-27 00:43:01 +0600

    togle

--- src/Dashboard/AddItem/AddItem.jsx
+++ src/Dashboard/AddItem/AddItem.jsx
@@ -2,13 +2,15 @@ import SectionTile from "../../Page/Section/SectionTile";
 import { useForm } from 'react-hook-form';
 
 const AddItem = () => {
+    const image_hosting_token = 'db6a93e47d6744ebc4432b0b80b64de4'
+    console.log(image_hosting_token);
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
     console.log(errors);
     return (
         <div>
             <div className="w-full px-10">
-                <SectionTile subHeading="What's new" heading="Add an item" ></SectionTile>
+                <SectionTile subHeading="What's new" heading="Addan item" ></SectionTile>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-control w-full mb-4">
                         <label className="label">
