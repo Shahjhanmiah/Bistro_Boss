1c131dbb57b6ef830a65c93b10d87aac90fb74bb
mdshahjhan <mdshahjha478877@gmail.com>
2023-10-29 22:50:12 +0600

    react

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
                 {/* Page content here */}
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden bg-black">Open drawer</label>
+                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden bg-black">Open The  drawer Dashbord</label>
                 <Outlet></Outlet>
 
             </div>
--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -7,50 +7,51 @@ import logo from '../../assets/assets/Tablet login-bro.png'
 import { AuthContext } from '../Context/AuthProvider';
 import Swal from 'sweetalert2'
 import SocialLogin from '../SocialLogin/SocialLogin';
+import ReCAPTCHA from "react-google-recaptcha";
 
 
 const Login = () => {
     // const [disabled, setDisabled] = useState(true)
 
-     const { login, } = useContext(AuthContext);
-     const navigate = useNavigate();
-      const location = useLocation();
+    const { login, } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
 
-      const from = location.state?.from?.pathname || "/";
+    const from = location.state?.from?.pathname || "/";
 
     useEffect(() => {
         loadCaptchaEnginge(6);
     }, [])
 
-    
 
 
-        const handleLogin = event => {
-            event.preventDefault();
-            const form = event.target;
-            const email = form.email.value;
-            const password = form.password.value;
-            console.log(email, password);
-            login(email, password)
-                .then(result => {
-                    const user = result.user;
-                    console.log(user);
-                    Swal.fire({
-                        title: 'User Login Successful.',
-                        showClass: {
-                            popup: 'animate__animated animate__fadeInDown'
-                        },
-                        hideClass: {
-                            popup: 'animate__animated animate__fadeOutUp'
-                        }
-                    });
-                    navigate(from, { replace: true });
-                })
-        }
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        login(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                Swal.fire({
+                    title: 'User Login Successful.',
+                    showClass: {
+                        popup: 'animate__animated animate__fadeInDown'
+                    },
+                    hideClass: {
+                        popup: 'animate__animated animate__fadeOutUp'
+                    }
+                });
+                navigate(from, { replace: true });
+            })
+    }
 
 
-            
-    
+
+
     // const handleValidateCaptcha = (e) => {
     //     const user_captcha_value = e.target.value;
     //     if (validateCaptcha(user_captcha_value)) {
@@ -73,8 +74,14 @@ const Login = () => {
     //             // setAuthToken(user);
     //         })
     //         .catch(err => console.error(err));
-    
+
     // }
+
+    // google reachapt 
+
+    function onChange(value) {
+        console.log("Captcha value:", value);
+      }
 
 
 
@@ -83,10 +90,11 @@ const Login = () => {
             <Helmet>
                 <title>Bistro Boss | Login</title>
             </Helmet>
+
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col md:flex-row-reverse">
                     <div className="text-center md:w-1/2 lg:text-left pl-10 ml-10">
-                        
+
                         <img src={logo} alt=''></img>
                     </div>
                     <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
@@ -116,17 +124,26 @@ const Login = () => {
                             </div>
                             {/* TODO: make button disabled for captcha */}
                             <div className="form-control mt-6">
-                               
-                                <input  className="btn btn-primary" type="submit" value="Login" />
+
+                                <input className="btn btn-primary" type="submit" value="Login" />
                             </div>
                         </form>
+                       <div className='mx-auto'>
+                       <ReCAPTCHA
+                        sitekey="6LfHcNwoAAAAAMyvSYahzeh--AkIRomm3qjugy0Z"
+                        onChange={onChange}
+                    />,
+                       </div>
                         <p className='text-center'><small>New Here? <Link to="/register">Create an account</Link> </small></p>
-                       <SocialLogin></SocialLogin>
+                        <SocialLogin></SocialLogin>
 
                     </div>
+                   
                 </div>
+                
             </div>
         </>
+        
     );
 };
 
