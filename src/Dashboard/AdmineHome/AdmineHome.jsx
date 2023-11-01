599866b4b816e20e66e11117d36af0a7c87adff7
mdshahjhan <mdshahjha478877@gmail.com>
2023-11-01 23:45:39 +0600

    stats

--- src/Dashboard/AdmineHome/AdmineHome.jsx
+++ src/Dashboard/AdmineHome/AdmineHome.jsx
@@ -52,8 +52,8 @@ const AdmineHome = () => {
                     <div className="stat-figure text-secondary ">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
                     </div>
-                    <div className="stat-title font-bold text-3xl text-blue-700">New User</div>
-                    <div className="stat-value">{stats.users}</div>
+                    <div className="stat-title font-bold text-3xl text-blue-700">Menu Items</div>
+                    <div className="stat-value">{stats.products}</div>
                     <div className="stat-desc">↗︎ 400 (22%)</div>
                 </div>
 
