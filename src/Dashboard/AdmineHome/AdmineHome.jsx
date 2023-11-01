dc4d4ab3ab50467b51e2081429b61bff3c84f12f
mdshahjhan <mdshahjha478877@gmail.com>
2023-11-01 23:43:45 +0600

    alluser

--- src/Dashboard/AdmineHome/AdmineHome.jsx
+++ src/Dashboard/AdmineHome/AdmineHome.jsx
@@ -30,9 +30,22 @@ const AdmineHome = () => {
                     <div className="stat-value">${stats.revenue}</div>
                     <div className="stat-desc">Jan 1st - Feb 1st</div>
                 </div>
-                
-            </div>
 
+            </div>
+            {/*  2 and */}
+            <div className="stats shadow">
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary ">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+                    </div>
+                    <div className="stat-title font-bold text-3xl text-blue-700">New User</div>
+                    <div className="stat-value">{stats.users}</div>
+                    <div className="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+
+            </div>
+            {/* 3 Thrad */}
             <div className="stats shadow">
 
                 <div className="stat">
@@ -43,7 +56,7 @@ const AdmineHome = () => {
                     <div className="stat-value">{stats.users}</div>
                     <div className="stat-desc">↗︎ 400 (22%)</div>
                 </div>
-                
+
             </div>
         </div>
     );
