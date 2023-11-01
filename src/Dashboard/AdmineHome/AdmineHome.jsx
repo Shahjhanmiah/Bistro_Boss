5752423bbc939ef2107d46cd6881e18cde894ae7
mdshahjhan <mdshahjha478877@gmail.com>
2023-11-01 23:46:44 +0600

    Temp

--- src/Dashboard/AdmineHome/AdmineHome.jsx
+++ src/Dashboard/AdmineHome/AdmineHome.jsx
@@ -50,7 +50,7 @@ const AdmineHome = () => {
 
                 <div className="stat">
                     <div className="stat-figure text-secondary ">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
                     </div>
                     <div className="stat-title font-bold text-3xl text-blue-700">Menu Items</div>
                     <div className="stat-value">{stats.products}</div>
@@ -58,6 +58,21 @@ const AdmineHome = () => {
                 </div>
 
             </div>
+            {/*  4 th */}
+
+            <div className="stats shadow">
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary ">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+                    </div>
+                    <div className="stat-title font-bold text-3xl text-blue-700">Menu Items</div>
+                    <div className="stat-value">{stats.products}</div>
+                    <div className="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+
+            </div>
+
         </div>
     );
 };
