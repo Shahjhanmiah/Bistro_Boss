1b49a67b0937ac51aac200db1fda0e8f1d1fe6ed
mdshahjhan <mdshahjha478877@gmail.com>
2023-11-02 00:54:14 +0600

    dashbord

--- src/Dashboard/AdmineHome/AdmineHome.jsx
+++ src/Dashboard/AdmineHome/AdmineHome.jsx
@@ -1,6 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hook/useAxiosSecure";
 import useAuth from "../useAuth";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+
 
 
 const AdmineHome = () => {
@@ -15,6 +18,33 @@ const AdmineHome = () => {
             return res.data
         }
     })
+
+    // chrate api in fontent api emplent create in api backend 
+
+    const { data: chartData = [] } = useQuery({
+        queryKey: ['chart-data'],
+        queryFn: async () => {
+            const res = await axisoSecure('/order-stats');
+            return res.data;
+        }
+    })
+
+
+    // cahar in custorme charte 
+    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+        Z`;
+    };
+
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
 
 
     return (
@@ -64,7 +94,7 @@ const AdmineHome = () => {
 
                 <div className="stat">
                     <div className="stat-figure text-secondary ">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
                     </div>
                     <div className="stat-title font-bold text-3xl text-blue-700">Ourders</div>
                     <div className="stat-value">{stats.orders}</div>
@@ -75,11 +105,32 @@ const AdmineHome = () => {
 
             <div className="flex">
                 <div className="w-1/2">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Bar dataKey="total" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {chartData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+
 
                 </div>
             </div>
 
-        </div>
+        </div >
     );
 };
 
