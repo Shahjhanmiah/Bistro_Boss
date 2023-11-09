fa9fab6501eac7b01f51b49a09b874af003e97c0
mdshahjhan <mdshahjha478877@gmail.com>
2023-11-09 10:02:13 +0600

    userHome

--- src/Dashboard/AdmineHome/UserHome.jsx
+++ src/Dashboard/AdmineHome/UserHome.jsx
@@ -21,7 +21,7 @@ const UserHome = () => {
 
             <div className="w-full m-4  ">
                 <h1 className="text-5xl text-center text-orange-500  ">Well Come Back,{user.name}</h1>
-                  <Helmet> Dashbord || UserHome</Helmet>
+                  <Helmet> <title>Dashbord || UserHome</title></Helmet>
                 <div>
                     <div className="stats shadow m-8 py-10 ">
 
